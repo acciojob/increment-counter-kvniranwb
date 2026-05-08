6521941fd844c7fe7baca79f77158b18638a1031
kvniranwb <niranjanvt967@gmail.com>
2026-05-08 14:35:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const counter = document.getElementById("counter");
 const btn = document.getElementById("incrementBtn");
 
 btn.addEventListener("click", function () {
-    alert(counter.textContent);
+    alert(Number(counter.textContent));
 
     counter.textContent = Number(counter.textContent) + 1;
 });
