ac869948177223c8d581031f1f7f5111b82c0655
kvniranwb <niranjanvt967@gmail.com>
2026-05-08 14:28:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+const counter = document.getElementById("counter");
+const btn = document.getElementById("incButton");
+
+btn.addEventListener("click", function () {
+    alert(counter.textContent);
+
+    counter.textContent = Number(counter.textContent) + 1;
+});
